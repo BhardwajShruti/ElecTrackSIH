03134d0cb45a7e2ec8b21a25aa56e5114111e5e3
BhardwajShruti <bhardwajshruti97@gmail.com>
2018-03-31 07:27:28 +0530

    version29/3_shru

--- app/src/main/java/com/example/android/electracksih/Device_Fragment.java
+++ app/src/main/java/com/example/android/electracksih/Device_Fragment.java
@@ -3,6 +3,8 @@ package com.example.android.electracksih;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -26,22 +28,30 @@ public class Device_Fragment extends android.support.v4.app.Fragment {
     RecyclerView.LayoutManager mLayoutManager;
     RecyclerView.LayoutManager mLayoutManager2;
     Switch onOff;
+    graph_frame gframe;
 int a;
     public Device_Fragment() {
-    }
-
-    ;
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.content_device__fragment, container, false);
+
+//        gframe= new graph_frame();
+//       // gframe.setArguments(bundle);
+//        FragmentManager fm= getActivity().getSupportFragmentManager();
+//        FragmentTransaction ft = fm.beginTransaction();
+//        ft.replace(R.id.graph_container, gframe);
+//        ft.commit();
+
+
         onOff = (Switch) view.findViewById(R.id.onOff);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.device_recycler_view_frag);
         // in content do not change the layout size of the RecyclerView
         //  mRecyclerView.setHasFixedSize(true);
-Bundle bundle = getArguments();
- a = bundle.getInt("id",0);
+        Bundle bundle = getArguments();
+        a = bundle.getInt("id", 0);
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this.getActivity(), LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -50,58 +60,59 @@ Bundle bundle = getArguments();
         mAdapter = new MyAdapter(this.getActivity());
         mRecyclerView.setAdapter(mAdapter);
 
-onOff.setOnClickListener(new View.OnClickListener() {
+        onOff.setOnClickListener(new View.OnClickListener() {
 
 
-
-
-
-    @Override
-    public void onClick(View v) {
-String mid;
-int stat;
-        boolean switch_checked =  onOff.isChecked();
-        if(switch_checked){
-           mid =  "lightturnedon";
-        stat = 1;
-        }
-        else{
-            mid = "lightturnedoff";
-        stat = 0;
-        }
+            @Override
+            public void onClick(View v) {
+                String mid;
+                int stat;
+                boolean switch_checked = onOff.isChecked();
+                if (switch_checked) {
+                    mid = "lightturnedon";
+                    stat = 1;
+                } else {
+                    mid = "lightturnedoff";
+                    stat = 0;
+                }
 
 
 //                String urlString="http://192.168.43.189:5000/lightturnedon?arg1=&arg2=&ar3=";
-        String urlString ="http://172.28.25.147:5000";
-        switch(a){
-            case 1:{
-                 urlString=urlString+"/"+mid+"?arg1=1&arg2=114&arg3="+stat;
-            }
-            case 2:{
-                urlString=urlString+"/"+mid+"?arg1=1&arg2=113&arg3="+stat;
+                String urlString = "http://172.28.25.147:5000";
+                switch (a) {
+                    case 1: {
+                        urlString = urlString + "/" + mid + "?arg1=1&arg2=114&arg3=" + stat;
+                    }
+                    case 2: {
+                        urlString = urlString + "/" + mid + "?arg1=1&arg2=113&arg3=" + stat;
 
-            }
-            case 3:{
-                urlString=urlString+"/"+mid+"?arg1=1&arg2=112&arg3="+stat;
+                    }
+                    case 3: {
+                        urlString = urlString + "/" + mid + "?arg1=1&arg2=112&arg3=" + stat;
 
-            }
-            case 4:{
-                urlString=urlString+"/"+mid+"?arg1=1&arg2=115&arg3="+stat;
+                    }
+                    case 4: {
+                        urlString = urlString + "/" + mid + "?arg1=1&arg2=115&arg3=" + stat;
 
-            }
+                    }
 
-        }
-        myTask myTask=new myTask(getContext(),urlString, new myTask.onSpecificStateChangeListener() {
-            @Override
-            public void onStateChanged(String string) {
-                Toast.makeText(getActivity(),string, Toast.LENGTH_SHORT).show();
+                }
+                myTask myTask = new myTask(getContext(), urlString, new myTask.onSpecificStateChangeListener() {
+                    @Override
+                    public void onStateChanged(String string) {
+                        Toast.makeText(getActivity(), string, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                myTask.execute(urlString);
             }
         });
-        myTask.execute(urlString);
-    }
-});
 
-
+        gframe= new graph_frame();
+        // gframe.setArguments(bundle);
+        FragmentManager fm= getActivity().getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.graph_container, gframe);
+        ft.commit();
         return view;
 
     }
---
+++ app/src/main/java/com/example/android/electracksih/graph_frame.java
@@ -0,0 +1,231 @@
+package com.example.android.electracksih;
+
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.scichart.charting.ClipMode;
+import com.scichart.charting.model.dataSeries.XyDataSeries;
+import com.scichart.charting.modifiers.AxisDragModifierBase;
+import com.scichart.charting.modifiers.ModifierGroup;
+import com.scichart.charting.visuals.SciChartSurface;
+import com.scichart.charting.visuals.annotations.HorizontalAnchorPoint;
+import com.scichart.charting.visuals.annotations.TextAnnotation;
+import com.scichart.charting.visuals.annotations.VerticalAnchorPoint;
+import com.scichart.charting.visuals.axes.IAxis;
+import com.scichart.charting.visuals.pointmarkers.EllipsePointMarker;
+import com.scichart.charting.visuals.renderableSeries.IRenderableSeries;
+import com.scichart.core.annotations.Orientation;
+import com.scichart.core.framework.UpdateSuspender;
+import com.scichart.core.model.DoubleValues;
+import com.scichart.drawing.utility.ColorUtil;
+import com.scichart.extensions.builders.SciChartBuilder;
+
+import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class graph_frame extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.graph_device, container, false);
+
+
+
+            // Create a SciChartSurface
+           final SciChartSurface surface = new SciChartSurface(this.getActivity());
+
+            // Get a layout declared in "activity_main.xml" by id
+
+            LinearLayout chartLayout = (LinearLayout) view.findViewById(R.id.chart_layout);
+
+            // Add the SciChartSurface to the layout
+            chartLayout.addView(surface);
+
+            // Initialize the SciChartBuilder
+            SciChartBuilder.init(this.getActivity());
+
+            // Obtain the SciChartBuilder instance
+            final SciChartBuilder sciChartBuilder = SciChartBuilder.instance();
+
+            // Create a numeric X axis
+            final IAxis xAxis = sciChartBuilder.newNumericAxis()
+                    .withAxisTitle("Time")
+                    .withVisibleRange(0,24)
+                    .build();
+
+            // Create a numeric Y axis
+            final IAxis yAxis = sciChartBuilder.newNumericAxis()
+                    .withAxisTitle("Power").withVisibleRange(0,3).build();
+
+            // Create a TextAnnotation and specify the inscription and position for it
+            TextAnnotation textAnnotation = sciChartBuilder.newTextAnnotation()
+                    .withX1(5.0)
+                    .withY1(55.0)
+                    .withText("")
+                    .withHorizontalAnchorPoint(HorizontalAnchorPoint.Center)
+                    .withVerticalAnchorPoint(VerticalAnchorPoint.Center)
+                    .withFontStyle(20, ColorUtil.Red)
+                    .build();
+
+            // Create interactivity modifiers
+            ModifierGroup chartModifiers = sciChartBuilder.newModifierGroup()
+                    .withPinchZoomModifier().withReceiveHandledEvents(true).build()
+                    .withZoomPanModifier().withReceiveHandledEvents(true).build()
+                    .build();
+
+            // Add the Y axis to the YAxes collection of the surface
+            Collections.addAll(surface.getYAxes(), yAxis);
+
+            // Add the X axis to the XAxes collection of the surface
+            Collections.addAll(surface.getXAxes(), xAxis);
+
+            // Add the annotation to the Annotations collection of the surface
+            Collections.addAll(surface.getAnnotations(), textAnnotation);
+
+            // Add the interactions to the ChartModifiers collection of the surface
+            Collections.addAll(surface.getChartModifiers(), chartModifiers);
+
+
+        IRenderableSeries lineSeries = sciChartBuilder.newLineSeries()
+                .withStrokeStyle(ColorUtil.LightBlue, 2f, true)
+                .build();
+
+        // Create an Ellipse PointMarker for the Scatter Series
+        EllipsePointMarker pointMarker = sciChartBuilder
+                .newPointMarker(new EllipsePointMarker())
+                .withFill(ColorUtil.LightBlue)
+                .withStroke(ColorUtil.Green, 2f)
+                .withSize(10)
+                .build();
+
+// Create and configure a scatter series
+        IRenderableSeries scatterSeries = sciChartBuilder.newScatterSeries()
+                .withPointMarker(pointMarker)
+                .build();
+
+//Adding Data
+ //  final     XyDataSeries lineData = sciChartBuilder.newXyDataSeries(Integer.class, Double.class).build();
+   //  final   XyDataSeries scatterData = sciChartBuilder.newXyDataSeries(Integer.class, Double.class).build();
+        final  XyDataSeries  lineData = sciChartBuilder.newXyDataSeries(Integer.class, Double.class).build();
+        final  XyDataSeries scatterData = sciChartBuilder.newXyDataSeries(Integer.class, Double.class).build();
+
+       final int dataCount =1000;
+        for (int i = 0; i < 1000; i++)
+        {
+            lineData.append(i, Math.sin(i * 0.1));
+            scatterData.append(i, Math.cos(i * 0.1));
+        }
+
+
+
+        // Create a couple of DataSeries for numeric (Int, Double) data
+
+
+
+// Create and configure a line series
+        lineSeries = sciChartBuilder.newLineSeries()
+                .withDataSeries(lineData)
+                .withStrokeStyle(ColorUtil.LightBlue, 2f, true)
+                .build();
+
+// Create an Ellipse PointMarker for the Scatter Series
+        pointMarker = sciChartBuilder
+                .newPointMarker(new EllipsePointMarker())
+                .withFill(ColorUtil.LightBlue)
+                .withStroke(ColorUtil.Green, 2f)
+                .withSize(10)
+                .build();
+
+// Create and configure a scatter series
+        scatterSeries = sciChartBuilder.newScatterSeries()
+                .withDataSeries(scatterData)
+                .withPointMarker(pointMarker)
+                .build();
+
+// Add a RenderableSeries onto the SciChartSurface
+        surface.getRenderableSeries().add(scatterSeries);
+        surface.getRenderableSeries().add(lineSeries);
+
+
+// Add a RenderableSeries onto the SciChartSurface
+        surface.getRenderableSeries().add(lineSeries);
+
+        //4
+        ModifierGroup additionalModifiers = sciChartBuilder.newModifierGroup()
+
+                .withPinchZoomModifier().build()
+
+                .withZoomPanModifier().withReceiveHandledEvents(true).build()
+
+                .withZoomExtentsModifier().withReceiveHandledEvents(true).build()
+
+                .withXAxisDragModifier().withReceiveHandledEvents(true).withDragMode(AxisDragModifierBase.AxisDragMode.Scale).withClipModex(ClipMode.None).build()
+
+                .withYAxisDragModifier().withReceiveHandledEvents(true).withDragMode(AxisDragModifierBase.AxisDragMode.Pan).build()
+
+                .build();
+//5
+        // Create a LegendModifier and configure a chart legend
+        ModifierGroup legendModifier = sciChartBuilder.newModifierGroup()
+                .withLegendModifier()
+                .withOrientation(Orientation.HORIZONTAL)
+                .withPosition(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 10)
+                .build()
+                .build();
+        ModifierGroup cursorModifier = sciChartBuilder.newModifierGroup()
+                .withCursorModifier().withShowTooltip(true).build()
+                .build();
+
+// Add the CursorModifier to the SciChartSurface
+        surface.getChartModifiers().add(cursorModifier);
+//=-------------------------------------------------------------------------
+// Add the LegendModifier to the SciChartSurface
+  //      surface.getChartModifiers().add(legendModifier);
+// Add the modifiers to the SciChartSurface
+    //    surface.getChartModifiers().add(additionalModifiers);
+//6
+
+        final DoubleValues lineDoubleData = new DoubleValues(dataCount);
+        final DoubleValues scatterDoubleData = new DoubleValues(dataCount);
+        lineDoubleData.setSize(dataCount);
+        scatterDoubleData.setSize(dataCount);
+
+        TimerTask updateDataTask = new TimerTask() {
+            private double _phaseShift = 0.0;
+            @Override
+            public void run() {
+                UpdateSuspender.using(surface, new Runnable() {
+                    @Override
+                    public void run() {
+                        // Fill the DoubleValues collections
+                        for (int i = 0; i < dataCount; i++)
+                        {
+                            lineDoubleData.set(i, Math.sin(i * 0.1 + _phaseShift));
+                            scatterDoubleData.set(i, Math.cos(i * 0.1 + _phaseShift));
+                        }
+                        // Update DataSeries using bunch update
+                        lineData.updateRangeYAt(0, lineDoubleData);
+                        scatterData.updateRangeYAt(0, scatterDoubleData);
+                    }
+                });
+                _phaseShift += 0.01;
+            }
+        };
+
+        Timer timer = new Timer();
+        long delay = 0;
+        long interval = 10;
+        timer.schedule(updateDataTask, delay, interval);
+
+    return  view;
+    }
+}
